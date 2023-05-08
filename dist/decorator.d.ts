9f4ced2edc1bb4be4a81a20b86bc360af55d3a44
Oriun <emmanuel.nuiro@epitech.eu>
2023-05-08 03:16:37 +0200

    [-] clean dist

--- dist/decorator.d.ts
+++
@@ -1,2 +0,0 @@
-import { Any, Schemas } from './types';
-export declare function API<Q extends Any = Any, P extends Any = Any, B extends Any = Any>(schemas?: Schemas<Q, P, B>): (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) => TypedPropertyDescriptor<any>;
--- dist/wrapper.d.ts
+++
@@ -1,5 +0,0 @@
-import { Schemas, Any, Handler } from "./types";
-import type { NextRequest } from "next/server";
-import z from "zod";
-export declare function api<Q extends Any = Any, P extends Any = Any, B extends Any = Any>(schemas: Schemas<Q, P, B>, handler: Handler<Q, P, B>): (request: NextRequest, variables: any) => Promise<Response>;
-export declare function parseQueryParams<T extends z.ZodType<any, any>>(request: NextRequest, schema: T): z.SafeParseReturnType<any, any>;
