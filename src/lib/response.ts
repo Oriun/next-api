0f0004041c12937aaa9101aa087da2fc3768e819
Oriun <emmanuel.nuiro@epitech.eu>
2023-11-22 13:39:13 +0100

    Update package version and add CORS headers to response

--- src/lib/response.ts
+++ src/lib/response.ts
@@ -15,6 +15,25 @@ const plainResponse = z.strictObject({
 
 const nullBodyStatuses = [101, 204, 205, 304];
 
+export function withCorsHeaders(response: Response) {
+    
+    const headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods":
+        "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH",
+      "Access-Control-Allow-Headers":
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    };
+
+    for (const [name, value] of Object.entries(headers)) {
+        if(response.headers.has(name)) continue
+        response.headers.set(name, value)
+    }
+    return response;
+  }
 
 export function withCookies(response: Response, cookies: Record<string, string>) {
 
@@ -28,35 +47,28 @@ export function withCookies(response: Response, cookies: Record<string, string>)
 export function responseParser(response: any) {
 
     if (response instanceof Response) {
-        return response
+        return withCorsHeaders(response)
     }
 
     if (typeof response !== "object") {
-        return new Response(JSON.stringify(response), {
-            headers: {
-                "content-type": "application/json",
-            }
-        })
+        return withCorsHeaders(new Response(JSON.stringify(response)))
     }
 
     const parsedRedirect = redirectResponse.safeParse(response)
     if (parsedRedirect.success) {
         const { status, redirect, cookies } = parsedRedirect.data
-        return withCookies(new Response(null, {
+        return withCookies(withCorsHeaders(new Response(null, {
             status,
             headers: {
+                "content-type": "text/plain",
                 location: redirect,
             },
-        }), cookies)
+        })), cookies)
     }
 
     const parsedResponse = plainResponse.safeParse(response)
     if (!parsedResponse.success) {
-        return new Response(JSON.stringify(response), {
-            headers: {
-                "content-type": "application/json",
-            }
-        })
+        return withCorsHeaders(new Response(JSON.stringify(response)))
     }
 
     const { data } = parsedResponse
@@ -64,12 +76,12 @@ export function responseParser(response: any) {
     const shouldHaveNullBody = nullBodyStatuses.includes(data.status);
     const body = data.body !== null ? shouldHaveNullBody ? null : JSON.stringify(data.body) : null;
 
-    return withCookies(new Response(body, {
+    return withCookies(withCorsHeaders(new Response(body, {
         status: data.status,
         headers: {
             "content-type": "application/json",
             ...data.headers,
         },
-    }), data.cookies);
+    })), data.cookies);
 
 }
